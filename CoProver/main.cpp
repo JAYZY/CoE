4f57e23fc32287f3cd16058d4a6f8e36586fa56f
jayzy <77367632@qq.com>
2017-12-13 13:52:05 +0800

    new project[zj]

---
+++ CoProver/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Sammy Guergachi <sguergachi at gmail.com>
+ *
+ * Created on 2017年12月13日, 下午1:50
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
